dac8eb3648dfe19d1361a0f9f3e08a9e36f49a9a
chelvivl <vcheley15@mail.ru>
2020-11-03 23:17:36 +0300

    improved swagger

--- src/main/java/com/mastery/java/task/rest/IndexController.java
+++ src/main/java/com/mastery/java/task/rest/IndexController.java
@@ -1,9 +1,11 @@
 package com.mastery.java.task.rest;
 
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Api(tags = "Start page", description = "Opening the start page index.html")
 public class IndexController {
 
     @GetMapping("/")
